14d3ed912790010ef9f37bdddfa506a2ef0cfbb0
Darryl <ddarling85@yahoo.com>
2019-10-26 20:54:28 -0400

    Updated centos elk build

--- conatinerWork/elk-build-centos.sh
+++ conatinerWork/elk-build-centos.sh
@@ -1,4 +1,4 @@
-## Uninstall previous Docker version if necessary
+echo '## Uninstall previous Docker version if necessary... \n\n'
 sudo yum remove docker \
                   docker-client \
                   docker-client-latest \
@@ -8,24 +8,33 @@ sudo yum remove docker \
                   docker-logrotate \
                   docker-engine -y
 
-## Install and enable Docker software
+echo '## Install Docker requirements and adding repo... \n\n'
 sudo yum install -y yum-utils   device-mapper-persistent-data   lvm2
-sudo yum install -y docker-ce docker-ce-cli containerd.io
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+echo '## Install and enable Docker software... \n\n'
 sudo yum install -y docker-ce docker-ce-cli containerd.io
 sudo systemctl enable docker
 sudo systemctl start docker
 
-## Add user to Docker group
+echo '## Add user to Docker group... \n\n'
 sudo usermod -aG docker $USER
 
-## Download docker-compose
+echo '## Download docker-compose... \n\n'
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 # docker-compose --version
 
-## Clone the ELK repo
+echo '## Add required volume mount points for elasticsearch containers... \n\n'
+sudo mkdir -p /srv/elasticsearch/es01_logs /srv/elasticsearch/es01_data /srv/elasticsearch/es02_data /srv/elasticsearch/es02_logs
+sudo chown -R 1000:1000 /srv/elasticsearch
+
+echo '## Add required vm.max_map_count for elasticsearch containers... \n\n'
+sudo sysctl -w vm.max_map_count=262144
+sudo sysctl -p
+
+echo '## Clone the ELK repo... \n\n'
 git clone https://github.com/ddarling85/docker-elk-clone.git
 chcon -R system_u:object_r:admin_home_t:s0 docker-elk-clone/
 cd docker-elk-clone
